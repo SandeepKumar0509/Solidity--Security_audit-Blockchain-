5924faf1b98086d70f6db28f9e1e315db8983f5e
SandeepKumar0509 <143065262+SandeepKumar0509@users.noreply.github.com>
2024-04-09 21:55:25 +0530

    Create README

---
+++ README.m
@@ -0,0 +1,37 @@
+# Solidity-Security-Audit
+
+## Security Audit for Solidity
+- **Slither**
+- **Solgraph**
+
+## Slither 
+```
+$ sudo apt install software-properties-common
+
+$ sudo add-apt-repository ppa:ethereum/ethereum
+
+$ sudo apt install solc
+
+$ pip3 install solc-select
+
+**Using pip**
+
+$ pip3 install slither-analyzer
+
+**Using GitHub**
+
+$ git clone <https://github.com/crytic/slither.git> && cd slither
+
+$ python3 setup.py install
+
+**Using Docker**
+
+$ docker pull trailofbits/eth-security-toolbox
+```
+#### Make a file
+```
+touch contractname.sol
+```
+```
+nano
+```
